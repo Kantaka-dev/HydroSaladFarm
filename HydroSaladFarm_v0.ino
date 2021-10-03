e79a369df782fd35f0625d2f7dd2042926c206dd
Kantaka-dev <64575382+Kantaka-dev@users.noreply.github.com>
2021-10-03 11:46:23 +0700

    create code v0
    define pins

---
+++ HydroSaladFarm_v0.ino
@@ -0,0 +1,22 @@
+#define RELAY_PIN_SOLENOID_PLANTLAYER1 12
+#define RELAY_PIN_SOLENOID_PLANTLAYER2 13
+#define RELAY_PIN_SOLENOID_MIXTURE      7
+#define RELAY_PIN_SOLENOID_WATERTANK    8
+#define RELAY_PIN_PUMP                  4
+
+#define MOTOR_PIN_NUTRIENTA             9
+#define MOTOR_PIN_NUTRIENTB             6
+#define MOTOR_PIN_PHUP                  5
+#define MOTOR_PIN_PHDOWN                3
+
+#define LED_PLANTLIGHT                  2 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
